083a2bb69df698bc0334609423001094870a5d28
MahdiSHams <cd /path/to/your/repository>
2026-04-30 14:24:36 +0330

    Update static web assets and compressed files for improved performance
    - Updated staticwebassets.build.json with new asset hashes and last write times.
    - Updated staticwebassets.build.json.cache to reflect the new hash.
    - Added a new compressed file for CSS assets to enhance loading efficiency.

--- obj/Debug/net10.0/WebSite.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd5a74fdd47121626faaa4d35ae5a4103ded28da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07db25f3bfdfd1f1949d8255315fe5c504cdeacc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
